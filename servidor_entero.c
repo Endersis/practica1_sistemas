0db5c3e679b59ef037f0e6fb853d701f0a0f1829
Endersis <121905995+Endersis@users.noreply.github.com>
2024-09-26 00:05:11 -0600

    Update servidor_entero.c

--- servidor_entero.c
+++ servidor_entero.c
@@ -9,56 +9,61 @@
 int main() {
     int server_fd, new_socket;
     struct sockaddr_in address;
-    int opt = 1;
     int addrlen = sizeof(address);
-    int buffer = 0;
+    int received_number, incremented_number;
     
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("socket failed");
+    // Crear socket
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd == -1) {
+        perror("Error al crear el socket");
         exit(EXIT_FAILURE);
     }
     
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
-    }
-    
+    // Configurar dirección del servidor
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
     
+    // Vincular socket
     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        perror("bind failed");
+        perror("Error en bind");
         exit(EXIT_FAILURE);
     }
     
+    // Escuchar conexiones
     if (listen(server_fd, 3) < 0) {
-        perror("listen");
+        perror("Error en listen");
         exit(EXIT_FAILURE);
     }
     
     printf("Servidor esperando conexiones...\n");
     
     while(1) {
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
-            perror("accept");
+        // Aceptar conexión
+        new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
+        if (new_socket < 0) {
+            perror("Error en accept");
             exit(EXIT_FAILURE);
         }
         
         printf("Cliente conectado\n");
         
         while(1) {
-            recv(new_socket, &buffer, sizeof(int), 0);
-            printf("Número recibido: %d\n", buffer);
+            // Recibir número
+            if (recv(new_socket, &received_number, sizeof(int), 0) <= 0) {
+                break;
+            }
+            printf("Número recibido: %d\n", received_number);
             
-            if(buffer == 0) {
+            if(received_number == 0) {
                 printf("Cliente envió 0. Cerrando conexión.\n");
                 break;
             }
             
-            buffer++;
-            send(new_socket, &buffer, sizeof(int), 0);
-            printf("Número enviado: %d\n", buffer);
+            // Incrementar y enviar de vuelta
+            incremented_number = received_number + 1;
+            send(new_socket, &incremented_number, sizeof(int), 0);
+            printf("Número enviado: %d\n", incremented_number);
         }
         
         close(new_socket);
